a65717b84c294e837f23301834cb8d86bb336392
Juti Buican <jbuican19@gmail.com>
2021-06-28 20:53:56 +0100

    Rendering as CollectionView using childView

--- app/app.js
+++ app/app.js
@@ -4,50 +4,64 @@ Backbone.$ = $;
 require('bootstrap');
 
 const Mn = require('backbone.marionette');
-// mn.$ = Backbone.$;
-$(document).ready(() => {
-  const myApp = new Mn.Application();
-  myApp.on('before:start', function () {
-    console.log('before:start');
-  });
+Mn.$ = Backbone.$;
 
-  myApp.on('start', function (opt) {
-    console.log('start');
-  });
+// START APP
+const myApp = new Mn.Application();
 
-  //models
-  myApp.Contact = Backbone.Model.extend({
-    defaults: {
-      firstName: 'Default Name',
-      phoneNumber: 'No phone number!',
-    },
-  });
-
-  myApp.ContactView = Mn.ItemView.extend({
-    template: '#contact-template',
-
-    events: {
-      'click p': 'showPhoneNumber',
-    },
-    showPhoneNumber: function () {
-      alert(this.model.escape('phoneNumber'));
-    },
-  });
-
-  const alice = new myApp.Contact({
-    firstName: 'Alice',
-    lastName: 'Doe',
-  });
-
-  const aliceView = new myApp.ContactView({
-    model: alice,
-  });
-
-  myApp.addRegions({
-    mainRegion: '#main-region',
-  });
-
-  myApp.mainRegion.show(aliceView);
-
-  myApp.start();
+// CREATE MODEL
+myApp.Contact = Backbone.Model.extend({
+  defaults: {
+    firstName: 'Default Name',
+    phoneNumber: 'No phone number!',
+  },
 });
+
+// CREATE COLLECTION
+myApp.ContactCollection = Backbone.Collection.extend({
+  model: myApp.Contact,
+});
+
+// *** display
+// CREATE ITEM VIEW
+myApp.ContactItemView = Mn.ItemView.extend({
+  template: '#contact-list-item',
+});
+
+// CREATE COLLECTION VIEW
+// Specify childView instead of itemView:
+myApp.ContactsView = Mn.CollectionView.extend({
+  tagName: 'ul',
+  // itemView: myApp.ContactItemView,
+  childView: myApp.ContactItemView,
+});
+
+// start contact myApp instant
+const contacts = new myApp.ContactCollection([
+  {
+    firstName: 'Bob',
+    lastName: 'Brigham',
+    phoneNumber: '555-0163',
+  },
+  {
+    firstName: 'Alice',
+    lastName: 'Arten',
+    phoneNumber: '555-0184',
+  },
+  {
+    firstName: 'Charlie',
+    lastName: 'Campbell',
+    phoneNumber: '555-0129',
+  },
+]);
+
+const contactsListView = new myApp.ContactsView({
+  collection: contacts,
+});
+
+myApp.addRegions({
+  mainRegion: '#main-region',
+});
+
+myApp.mainRegion.show(contactsListView);
+myApp.start();
