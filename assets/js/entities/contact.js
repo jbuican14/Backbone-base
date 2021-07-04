8c6b3f0ea20b1fc79e491567afef7a6c297cc5b7
Juti Buican <jbuican19@gmail.com>
2021-07-04 21:22:34 +0100

    Added contacts

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -19,6 +19,18 @@ contactApp.module(
           lastName: 'Arten',
           phoneNumber: '555-0184',
         },
+        {
+          id: 2,
+          firstName: 'Bob',
+          lastName: 'Brigham',
+          phoneNumber: '555-0163',
+        },
+        {
+          id: 3,
+          firstName: 'Charlie',
+          lastName: 'Campbell',
+          phoneNumber: '555-0129',
+        },
       ]);
     };
 
